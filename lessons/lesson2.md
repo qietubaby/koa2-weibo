475cfd951f94c9e08e0c0717fd6046f5f5cef7da
qietubaby <201951244@qq.com>
2020-03-03 16:13:32 +0800

    refactor:调整结构目录

--- lessons/lesson2.md
+++ lessons/lesson2.md
@@ -2,3 +2,5 @@
 ### npm -e koa2-weibo
 ### npm run dev
 ### 浏览器访问localhost:3000
+
+### 安装cross-env设置环境变量
--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
@@ -25,12 +25,12 @@ app.use(views(__dirname + '/views', {
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
